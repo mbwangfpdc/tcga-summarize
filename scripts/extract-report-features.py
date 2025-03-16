a1cc9e422c5004e8ff09d2e4e84927febe764245
Morgan Borjigin-Wang <mbwang10@gmail.com>
2025-03-16 18:32:43 -0500

    Final upload. Haven't tested since figures were complete, so maybe some bugs to work out

--- scripts/extract-report-features.py
+++ scripts/extract-report-features.py
@@ -31,7 +31,7 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--model",
-        choices=["biomistral", "conch", "llama"],
+        choices=["biomistral", "mistral", "conch", "llama"],
         default="biomistral",
         help="Foundation model to use to extract report features.",
     )
@@ -46,51 +46,38 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
+def mistral_extract(repo_root: str, reports: list) -> np.ndarray:
+    return mistral_extract_impl(repo_root=repo_root, reports=reports, model_name="Mistral-7B-Instruct-v0.1", hd_path="mistralai/Mistral-7B-Instruct-v0.1")
+
 def biomistral_extract(repo_root: str, reports: list) -> np.ndarray:
+    return mistral_extract_impl(repo_root=repo_root, reports=reports, model_name="BioMistral-7B", hd_path="BioMistral/BioMistral-7B")
+
+def mistral_extract_impl(repo_root: str, reports: list, model_name: str, hd_path: str) -> np.ndarray:
     from transformers import MistralModel
     from vllm import LLM
 
-    biomistral_path = os.path.join(repo_root, "models", "BioMistral-7B")
-    if not os.path.exists(biomistral_path):  # not found locally
-        biomistral_path = "BioMistral/BioMistral-7B"  # fall back to HF
+    mistral_path = os.path.join(repo_root, "models", model_name)
+    if not os.path.exists(mistral_path):  # not found locally
+        mistral_path = hd_path
 
-    biomistral_base = os.path.join(repo_root, "models", "BioMistral-7B-base")
-    if not os.path.exists(biomistral_base):  # need to sanitize state dict
-        # BioMistral on HF is configured as MistralForCausalLM
+    mistral_base = os.path.join(repo_root, "models", f"{model_name}-Base")
+    if not os.path.exists(mistral_base):  # need to sanitize state dict
+        # Mistral on HF is configured as MistralForCausalLM
         # and the transformer weights are prefixed with "model.".
         # vLLM requires the model be configured as MistralModel for embeddings
         # so load using huggingface (which takes care of weight prefixes too)
         # and save just the transformer backbone model.
-        temp = MistralModel.from_pretrained(biomistral_path, torch_dtype=torch.bfloat16)
-        temp.save_pretrained(biomistral_base, safe_serialization=False)  # TODO errors?
+        temp = MistralModel.from_pretrained(mistral_path, torch_dtype=torch.bfloat16)
+        temp.save_pretrained(mistral_base, safe_serialization=False)  # TODO errors?
         del temp
 
-    model = LLM(model=biomistral_base, tokenizer=biomistral_path, enforce_eager=True)
+    model = LLM(model=mistral_base, tokenizer=mistral_path, enforce_eager=True)
     outputs = model.encode(reports)
     embeds = np.asarray([o.outputs.embedding for o in outputs], dtype=np.float32)
     return embeds
 
 def llama_extract(repo_root: str, reports: list) -> np.ndarray:
-    from transformers import LlamaModel
-    from vllm import LLM
-
-    llama_path = "meta-llama/Llama-3.1-8B-Instruct"
-
-    # biomistral_base = os.path.join(repo_root, "models", "BioMistral-7B-base")
-    # if not os.path.exists(biomistral_base):  # need to sanitize state dict
-    #     # BioMistral on HF is configured as MistralForCausalLM
-    #     # and the transformer weights are prefixed with "model.".
-    #     # vLLM requires the model be configured as MistralModel for embeddings
-    #     # so load using huggingface (which takes care of weight prefixes too)
-    #     # and save just the transformer backbone model.
-    #     temp = MistralModel.from_pretrained(llama_path, torch_dtype=torch.bfloat16)
-    #     temp.save_pretrained(biomistral_base, safe_serialization=False)  # TODO errors?
-    #     del temp
-
-    model = LLM(model=llama_path, tokenizer=llama_path, enforce_eager=True)
-    outputs = model.encode(reports)
-    embeds = np.asarray([o.outputs.embedding for o in outputs], dtype=np.float32)
-    return embeds
+    raise NotImplementedError("No LlamaModel supported, only LlamaForCausalLM")
 
 
 def conch_extract(repo_root: str, reports: list, batch_size: int) -> np.ndarray:
@@ -112,6 +99,9 @@ def conch_extract(repo_root: str, reports: list, batch_size: int) -> np.ndarray:
         batch_reports = reports[i : i + batch_size]
         with torch.inference_mode():
             tokens = tokenize(texts=batch_reports, tokenizer=tokenizer).to("cuda")
+            print(tokens.shape)
+            print(tokens.T[tokens.T.shape[0] - 5:].T[:10])
+            exit(0)
             batch_embeds = model.encode_text(tokens, normalize=True, embed_cls=True)
             batch_embeds = batch_embeds.detach().cpu().numpy()
             embeds.append(batch_embeds)
@@ -126,28 +116,36 @@ if __name__ == "__main__":
         args.repo_root = os.path.dirname(os.path.dirname(__file__))
 
     report_df = pd.read_csv(args.input_csv)
-    assert "case_id" in report_df and "text" in report_df
+    assert "case_id" in report_df
+    text_cols = [col for col in report_df.columns if col.startswith("text")]
+    assert len(text_cols) > 0
+    for text_col in text_cols:
+        report_slice = report_df[["case_id", text_col]]
+        if args.model == "biomistral":
+            embeds = biomistral_extract(
+                repo_root=args.repo_root,
+                reports=report_slice[text_col].to_list(),
+            )
+        elif args.model == "mistral":
+            embeds = mistral_extract(
+                repo_root=args.repo_root,
+                reports=report_slice[text_col].to_list(),
+            )
+        elif args.model == "conch":
+            embeds = conch_extract(
+                repo_root=args.repo_root,
+                reports=report_slice[text_col].to_list(),
+                batch_size=args.batch_size,
+            )
+        elif args.model == "llama":
+            embeds = llama_extract(
+                repo_root=args.repo_root,
+                reports=report_slice[text_col].to_list(),
+            )
+        else:
+            raise ValueError(f"Unknown model type: {args.model}")
 
-    if args.model == "biomistral":
-        embeds = biomistral_extract(
-            repo_root=args.repo_root,
-            reports=report_df["text"].to_list(),
-        )
-    elif args.model == "conch":
-        embeds = conch_extract(
-            repo_root=args.repo_root,
-            reports=report_df["text"].to_list(),
-            batch_size=args.batch_size,
-        )
-    elif args.model == "llama":
-        embeds = llama_extract(
-            repo_root=args.repo_root,
-            reports=report_df["text"].to_list(),
-        )
-    else:
-        raise ValueError(f"Unknown model type: {args.model}")
-
-    out_adata = AnnData(obs=pd.DataFrame(index=report_df["case_id"].to_list()))
-    out_adata.obsm[f"X_{args.feature_label}"] = embeds
-    out_adata.uns["model_text"] = args.model
-    out_adata.write_h5ad(args.output_h5ad)
+        out_adata = AnnData(obs=pd.DataFrame(index=report_slice["case_id"].to_list()))
+        out_adata.obsm[f"X_{args.feature_label}"] = embeds
+        out_adata.uns["model_text"] = args.model
+        out_adata.write_h5ad(f"{args.output_h5ad}.{text_col}")
