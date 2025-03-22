ba760647e2f27b963d053cb55b68f8caf74a6a6a
Morgan Borjigin-Wang <mbwang10@gmail.com>
2025-03-22 18:22:35 -0500

    touch up summarize script

--- summarize.py
+++ summarize.py
@@ -15,9 +15,17 @@ OUTPUT_DIR = "output"
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="llama", choices=["bm", "llama"])
+    parser.add_argument("--testonly", action="store_true", default=False)
+    parser.add_argument("--reports")
+    parser.add_argument("--out", default="TCGA_Reports_Transformed.csv")
     return parser.parse_args()
 
 args = parse_args()
+
+# Load reports
+reports_path = args.reports
+reports = reports_from_file(reports_path)
+random.shuffle(reports)
 
 model_map = {
     "bm": "BioMistral/BioMistral-7B",
@@ -28,15 +36,8 @@ args.model = model_map[args.model]
 
 # Load model
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
-token = "CENSORED"
 model = args.model
-# tokenizer = AutoTokenizer.from_pretrained(model)
 llm = LLM(model=model, enforce_eager=True)
-
-# Load reports
-reports_path = os.path.join(DATA_DIR, "TCGA_Reports_Processed.csv")
-reports = reports_from_file(reports_path)
-random.shuffle(reports)
 
 PROMPT = "Extract and repeat the results of the following clinical report in a single paragraph. Focus on test results, diagnoses and clinical history. Omit the gross description. Do not acknowledge this prompt."
 def make_prompt_for_chat(case: str, report: str) -> str:
@@ -45,10 +46,9 @@ def make_prompt_for_chat(case: str, report: str) -> str:
 # Mistral models don't do well with the chat history
 chat_preprompt = []
 
-testonly = True
-NUM = 20 if testonly else len(reports)
+NUM = 20 if args.testonly else len(reports)
 convos = [deepcopy(chat_preprompt) for _ in range(NUM)]
-if testonly:
+if args.testonly:
     # For tests, shuffle the reports so we can do a diversity of testing
     myreports = deepcopy(reports)
     random.shuffle(myreports)
@@ -59,7 +59,7 @@ for convo, (case, report) in zip(convos, myreports):
 sampling_params = SamplingParams(max_tokens=1000, temperature=0, repetition_penalty=1.1)
 outputs = llm.chat(messages=convos, sampling_params=sampling_params)
 
-if testonly:
+if args.testonly:
     reset_compare()
     for output in outputs:
         prompt = output.prompt
@@ -67,8 +67,9 @@ if testonly:
         write_for_compare(prompt, generated_text)
 else:
     new_reports = []
-    for (case, _), output in outputs:
+    for output, (case, _) in zip(outputs, myreports):
         generated_text = output.outputs[0].text
         generated_text = generated_text.replace("\n", " ")
         new_reports.append((case, generated_text))
-    reports_to_file("TCGA_Reports_Transformed.csv", new_reports)
+    reports_to_file(args.out, new_reports)
+print("Done!")
